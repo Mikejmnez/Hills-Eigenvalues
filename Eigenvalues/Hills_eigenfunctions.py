3bed0d4924868520d8d5fc28bf8e5d0a2a2c57a9
Mikejmnez <mjimen17@jh.edu>
2021-04-17 18:47:48 -0400

    correct reference passed to matrix construction

--- Eigenvalues/Hills_eigenfunctions.py
+++ Eigenvalues/Hills_eigenfunctions.py
@@ -6,16 +6,16 @@ import numpy as _np
 from eig_system import matrix_system, eig_pairs
 
 
-def A_coefficients(q, N, alphas, K, cosine=True):
+def A_coefficients(q, N, coeffs, K, symmetry='None', case='None'):
     """ Returns the (sorted) eigenvalues and orthonormal eigenvectors of
     Hill's equation.
 
     Input:
         q: 1d-array, all elements must be imaginary and in ascending order.
         N: size of (square) Matrix
-        alphas: 1d-array, containing Fourier coefficients associated with
+        coeffs: 1d-array, containing Fourier coefficients associated with
             periodic coefficient in Hill's equation.
-        K: range(1, M, d). M is highest harmonic in alphas. d is either 1 or
+        K: range(1, M, d). M is highest harmonic in coeffs. d is either 1 or
             two. if d=1, Fourier sum is : cos(y)+cos(2*y)+... if d=2, the sum
             : cos(y)+cos(3*y)+cos(5*y)...
         cosine: True (default). This has to do with Fourier approx to periodic
@@ -30,20 +30,22 @@ def A_coefficients(q, N, alphas, K, cosine=True):
     if q.imag.any() == 0:
         raise Warning("q must be imaginary")
     for n in range(N):
-        a = eig_pairs(matrix_system(q[0], N, alphas, K, cosine))
+        a = eig_pairs(matrix_system(q[0], N, coeffs, K, symmetry, case))
         a = [a[n]]  # makes a list of the nth eigenvalue
         # As = Anorm(A[:, n], type, period)
         # As = As[_np.newaxis, :]
         for k in range(1, len(q)):
-            an = eig_pairs(matrix_system(q[k], N, alphas, K, cosine))
+            an = eig_pairs(matrix_system(q[k], N, coeffs, symmetry, case))
             a.append(an[n])
             # nA = Anorm(A[:, n], type, period)
             # nAs = nA[_np.newaxis, :]
             # As = _np.append(As, nAs, axis=0)
         # As = Fcoeffs(As, n, q, flag=imag)
-        if cosine is True:
+        if symmetry is 'None':
             vals.update({'a' + str(2 * n): _np.array(a)})
-        else:
+        elif symmetry is 'even':
+            vals.update({'a' + str(2 * n): _np.array(a)})
+        elif symmetry is 'odd':
             vals.update({'b' + str(2 * (n + 1)): _np.array(a)})
         # vals.update({'A' + str(2 * n): As})
     return vals
