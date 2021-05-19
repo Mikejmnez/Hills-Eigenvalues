e306e6374e19bac0ba4899c8b86a8125bccff3cf
Mikejmnez <mjimen17@jh.edu>
2021-05-19 01:08:40 -0400

    class fn to calculate even eigenfunctions

--- Eigenvalues/Hills_eigenfunctions.py
+++ Eigenvalues/Hills_eigenfunctions.py
@@ -39,13 +39,21 @@ class eigenfunctions:
         vals = {}
         if Ncut != 0:
             N = Ncut
-        leng = range(N)
+        LEN = range(N)
         for n in leng:
-            terms = [_np.cos((2*k)*x)*(As['A'+str(2*n)][0, k]) for k in leng]
+            terms = [_np.cos((2*k)*x)*(As['A'+str(2*n)][0, k]) for k in LEN]
             vals.update({'phi' + str(2 * n): + _np.sum(terms, axis=0)})
             vals.update({'phi' + str(2 * n):
                          vals['phi' + str(2 * n)][_np.newaxis, :]})
-        pass
+            vals.update({'a' + str(2 * n): As['a' + str(2 * n)]})
+        for i in range(1, len(q)):
+            for n in LEN:
+                terms = [_np.cos((2*k)*x)*(As['A'+str(2*n)][i, k]) for k in LEN]
+                phi = np.sum(terms, axis=0)
+                phi = phi[_np.newaxis, :]
+                phi = _np.append(vals['phi' + str(2 * n)], ce, axis=0)
+                vals.update({'phi' + str(2 * n): ce})
+        return vals
 
 
 def A_coefficients(q, N, coeffs, K, symmetry='None', case='None'):
