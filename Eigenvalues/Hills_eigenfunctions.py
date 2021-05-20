f8f039c090f293f88b3f54bd21eabc663b382002
Mikejmnez <mjimen17@jh.edu>
2021-05-20 18:25:27 -0400

    flip Fcoeffs around eigval cross

--- Eigenvalues/Hills_eigenfunctions.py
+++ Eigenvalues/Hills_eigenfunctions.py
@@ -671,6 +671,7 @@ def gaussCoeffs(As, n, q):
     if n == 2:
         # Identify the two maxima (like two EPs).
         mm = _np.where(As[:, n].real == _np.max(As[:, n].real))[0][0]  # EP-ish
+        nn = _np.where(q.imag <= qs[n])[0][-1]
         As[mm + 1:, 0].real = - As[mm + 1:, 0].real
         As[mm + 1:, 2].imag = - As[mm + 1:, 2].imag
         As[mm + 1:, 4].real = - As[mm + 1:, 4].real
@@ -683,8 +684,26 @@ def gaussCoeffs(As, n, q):
         As[mm + 1:, 11].real = - As[mm + 1:, 11].real
         As[mm + 1:, 12].real = - As[mm + 1:, 12].real
 
+        As[nn + 1:, 0].imag = -As[nn + 1:, 0].imag
+        As[nn + 1:, 1].imag = - As[nn + 1:, 1].imag
+        As[nn + 1:, 2].imag = - As[nn + 1:, 2].imag
+        As[nn + 1:, 3].real = - As[nn + 1:, 3].real
+        As[nn + 1:, 4] = -As[nn + 1:, 4]
+        As[nn + 1:, 5].imag = -As[nn + 1:, 5].imag
+        As[nn + 1:, 6].imag = -As[nn + 1:, 6].imag
+        As[nn + 1:, 7].imag = -As[nn + 1:, 7].imag
+        As[nn + 1:, 8].imag = -As[nn + 1:, 8].imag
+        As[nn + 1:, 9].imag = -As[nn + 1:, 9].imag
+        As[nn + 1:, 10].imag = -As[nn + 1:, 10].imag
+        As[nn + 1:, 11].imag = -As[nn + 1:, 11].imag
+        As[nn + 1:, 12].imag = -As[nn + 1:, 12].imag
+        As[nn + 1:, 13] = -As[nn + 1:, 13]
+        As[nn + 1:, 14] = -As[nn + 1:, 14]
+        As[nn + 1:, 15] = -As[nn + 1:, 15]
+
     if n == 3:
         mm = _np.where(As[:, n].real == _np.max(As[:, n].real))[0][0]  # EP-ish
+        nn = _np.where(q.imag <= qs[n])[0][-1]  # next EP-like
         As[mm + 1:, 0].real = - As[mm + 1:, 0].real
         As[mm + 1:, 1].real = - As[mm + 1:, 1].real
         As[mm + 1:, 2].real = - As[mm + 1:, 2].real
@@ -713,6 +732,19 @@ def gaussCoeffs(As, n, q):
         As[ll[0]:ll[-1] + 1, 9].real = -As[ll[0]:ll[-1] + 1, 9].real
         As[ll[0]:ll[-1] + 1, 10].real = -As[ll[0]:ll[-1] + 1, 10].real
         As[ll[0]:ll[-1] + 1, 11].real = -As[ll[0]:ll[-1] + 1, 11].real
+
+        As[nn + 1:, 0].imag = - As[nn + 1:, 0].imag
+        As[nn + 1:, 1].imag = - As[nn + 1:, 1].imag
+        As[nn + 1:, 2].imag = - As[nn + 1:, 2].imag
+        As[nn + 1:, 4].real = - As[nn + 1:, 4].real
+        As[nn + 1:, 5] = - As[nn + 1:, 5]
+        As[nn + 1:, 6] = - As[nn + 1:, 6]
+        As[nn + 1:, 7].imag = - As[nn + 1:, 7].imag
+        As[nn + 1:, 8].imag = - As[nn + 1:, 8].imag
+        As[nn + 1:, 9].imag = - As[nn + 1:, 9].imag
+        As[nn + 1:, 10].imag = - As[nn + 1:, 10].imag
+        As[nn + 1:, 11].imag = - As[nn + 1:, 11].imag
+        As[nn + 1:, 12].imag = - As[nn + 1:, 12].imag
 
     return As
 
