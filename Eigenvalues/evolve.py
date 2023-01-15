fe695f65a665378b4e025c9f7eed9ea08bd72295
Miguel Jimenez <mjimen17@jhu.edu>
2023-01-14 22:55:03 -0500

    shear flow solution, when flow has odd FS

--- Eigenvalues/evolve.py
+++ Eigenvalues/evolve.py
@@ -5,12 +5,14 @@ Module that evolves an initial condition contains class of stirring velocity fie
 import numpy as _np
 from Hills_eigenfunctions import (
 	complement_dot,
-	A_coefficients,
-	)
+	A_coefficients)
+from time_varying import _coeff_project
 from Hills_eigenfunctions import eigenfunctions as _eigfns
 import copy as _copy
 import xarray as _xr
 import xrft as _xrft
+
+_xrda_type = _xr.core.dataarray.DataArray
 
 class planarflows:
 	"""
@@ -23,14 +25,14 @@ class planarflows:
 	def shear_flow(
 		cls,
 		U = None,
-		bcs = None,
-		ic = None,
+		BCs = None,
+		IC = None,
 		kappa = None,
 		t0=None,
 		tf=None,
-		amp=None,
-		phase=None,
-		rotation=None,
+		Amp=None,
+		Phase=None,
+		tau=None,
 		Q=None,
 		delta=None,
 		)
@@ -42,10 +44,10 @@ class planarflows:
 	-----------
 		U: 1d array-like. xarray.dataarray.
 			Defines the shear flow as U(y). Only the cross-stream domain is defined.
-		bcs: int
+		BCs: int
 			Cross-stream boundary conditions to be satisfied by analytical solution.
 			Periodic or Neumann.
-		ic: None, or 2d xarray.dataarray.
+		IC: None, or 2d xarray.dataarray.
 			Initial condition. The domain must be consistent with that in U(y). When
 			`None`, 
 		kappa: float, list
@@ -55,16 +57,15 @@ class planarflows:
 			Initial time. If `None`, then default is zero.
 		tf: float.
 			Final time. If `None`, then default is 1 diffusive timescale unit.
-		amp: None, int or 1d-array like.
+		Amp: None, int or 1d-array like.
 			Time dependency of the shear flow's amplitude. Sign can change. If `None`
 			then default is unit value 1.
-		phase: None, int or 1d-array like.
+		Phase: None, int or 1d-array like.
 			Phase of shear flow. If None, then default is zero.
-		rotation: None, or 1d-array.
-			If None, plane parallel shear flow is always unidirectional. Its amplitude
-			and phase can vary. The renewing (renovating) flow can is defined by an
-			array of ones and zeros. A zero implies no rotation, and a one implies a
-			rotation of the velocity field by 90 degrees. The variable `amp` determines
+		tau: `None`, or float.
+			If `None`, the plane parallel shear flow is always unidirectional. Its
+			amplitude and phase can still vary. If `float`, 1/tau defines the
+			frequency at which the shear flow rotates. The variable `amp` determines
 			the sign of the velocity field.
 		Q: None, or 2d xarray.dataarray.
 			Defines the spatial forcing, separable along its independent coordinates. 
@@ -74,11 +75,58 @@ class planarflows:
 			the cross-stream domain.
 		delta: None, float or 1d array-like.
 			Frequency of forcing. When `None` there is no forcing (even when Q is 
-			given). If 1d array-like, len(delta) ==. number of forcing functions.
+			given). If 1d array-like, len(delta) == number of forcing functions.
 	"""
 
-	
-	
+	y = U.y.data. # extract coordinate - 0 to 2\pi
+	even_coeffs, odd_coeffs, *a = _coeff_project(U, y)
+
+	# need to assert that U is defined by an even Fourier series. Otherwise, 
+	# need to extend periodically so that it has an even Fourier series.
+
+	if _np.max(abs(odd_coeffs)) > 1e-7:  # I can make this smaller
+		Uold = _copy.deepcopy(U)
+		U = _xr.DataArray(Uold, dims=('y',), coords={'y': 2*y})
+		# make periodic extension
+		U[U.y > 2*_np.pi] = 1 - U[U.y > 2*_np.pi]
+
+		# velocity should only have even Fourier series
+		# max amplitude of shear flow is retained
+		# but domain is effectively halved. 
+
+		alphas_m, *a = _coeff_project(U, y)  # Fourier coeffs for shear flow
+
+		# effective Peclet number due to domain halving
+		Pe = 2 * Pe 
+		odd_flow = True # flag that will restore the dimensional value of Pe
+	else:
+		odd_flow = False
+
+	#  initial condition
+	if type(IC) == _xrda_type:  # one ic is given
+		fx = IC['f(x)'] # along-stream component of ic.
+		g = IC['g(y)'] # cross-stream component of ic.
+
+		Ck = _xrft.fft(fx, true_phase=True, true_amplitude=True)
+		Ck = Ck.rename({'freq_x':'k'}) # label k the wavenumber 
+
+		Kn = Ck['k'].values # wavenumber vals
+
+		da_y = _xrft.fft(g, true_phase=True, true_amplitude=True)
+		da_y = da_y.rename({'freq_y':'l'})
+
+		even_coeffs, odd_coeffs, *a = _coeff_project(da_y, y / 2)
+
+		acoords = {'r':range(len(even_coeffs.data))}
+		bcoords = {'r':range(len(odd_coeffs.data)-1)}
+
+		a_alps_y = _xr.DataArray(even_coeffs.data, coords=acoords, dims='r')
+		b_alps_y = _xr.DataArray(odd_coeffs.data[1:], coords=bcoords, dims='r')
+
+
+
+
+
 	pass
 
 
