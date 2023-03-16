ae785a4f8ae049441861263caf1b377d63f195aa
Miguel Jimenez <mjimen17@jhu.edu>
2023-03-16 14:34:05 -0400

    create file

---
+++ Eigenvalues/utils.py
@@ -0,0 +1,12 @@
+"""
+Contains useful information about exceptional points of some shear flows of interest
+"""
+import numpy as _np
+print('Hello')
+
+
+def cosine_shear(n=1):
+	"""
+	returns the location of EPs associated with a shear flow of multiplicity n.
+	"""
+	
