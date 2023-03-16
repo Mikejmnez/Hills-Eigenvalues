624dac201f900ccc88ed453f1881f9b84df1ab6a
Miguel Jimenez <mjimen17@jhu.edu>
2023-03-16 15:07:05 -0400

    include next EP

--- Eigenvalues/utils.py
+++ Eigenvalues/utils.py
@@ -8,3 +8,14 @@ def cosine_shear(n=1):
 	"""
 	returns the location of EPs associated with a shear flow of multiplicity n.
 	"""
+
+	if n==1:
+		qs = [2.938438, 32.942942, 95.613113, 190.950950, 318.959459, 479.636636,
+			  672.983483, 898.998498, 1157.684684, 1449.039039, 1773.061561, 2129.755255,
+			  2519.69,
+		]
+
+	else:
+		qs = 0
+
+	return qs
