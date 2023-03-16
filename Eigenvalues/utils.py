c1a8c709fa5cec0f9965711c08bbd1afe00b9b61
Miguel Jimenez <mjimen17@jhu.edu>
2023-03-16 14:34:19 -0400

    remove unnecessary line

--- Eigenvalues/utils.py
+++ Eigenvalues/utils.py
@@ -2,11 +2,9 @@
 Contains useful information about exceptional points of some shear flows of interest
 """
 import numpy as _np
-print('Hello')
 
 
 def cosine_shear(n=1):
 	"""
 	returns the location of EPs associated with a shear flow of multiplicity n.
 	"""
-	
